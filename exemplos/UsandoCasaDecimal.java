de9f8ca52889d6f0e38472b10d9dd032a83c36b1
Kauan <kceliass@gmail.com>
2020-07-30 20:37:45 -0300

    mais exemplos

---
+++ exemplos/UsandoCasaDecimal.java
@@ -0,0 +1,11 @@
+public class UsandoCasaDecimal{
+	public static void main(String[] args){
+        String nome;
+        double altura;
+
+        nome = "Kauan Elias";
+        altura = 1.83;
+
+        System.out.println("Oi, meu nome é " + nome + ", e tenho " + altura + " metros");
+	}
+}
---
+++ exemplos/UsandoTexto.java
@@ -0,0 +1,7 @@
+public class UsandoTexto{
+	public static void main(String[] args){
+        String nome;
+        nome = "Kauan Elias";
+        System.out.println("Meu nome e: " + nome);
+	}
+}
