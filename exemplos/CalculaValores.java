735443e7e857317f79928234b9c24f075e8d6d04
Kauan <kceliass@gmail.com>
2020-07-30 20:57:14 -0300

    operadores aritmeticos adicionados

---
+++ exemplos/CalculaValores.java
@@ -0,0 +1,26 @@
+public class CalculaValores{
+
+	public static void main(String[] args){
+
+        int numeroUm;
+        int numeroDois;
+        int soma;
+        int subtracao;
+        int multiplicacao;
+        int divisao;
+        
+        numeroUm = 10;
+        numeroDois = 60;
+
+        soma = numeroUm + numeroDois;
+        subtracao = numeroUm - numeroDois;
+        multiplicacao = numeroUm * numeroDois;
+        divisao = numeroUm / numeroDois;
+
+        System.out.println("O resultado da soma: " + soma);
+        System.out.println("O resultado da subtracao: " + subtracao);
+        System.out.println("O resultado da multiplicacao: " + multiplicacao);
+        System.out.println("O resultado da divisao: " + divisao);
+        
+	}
+}
