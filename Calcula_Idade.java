2169e6792059f92087885b37eb5c0fafbd755d84
Kauan <kceliass@gmail.com>
2020-07-30 19:51:54 -0300

    calcula a idade

---
+++ Calcula_Idade.java
@@ -0,0 +1,15 @@
+public class Calcula_Idade{
+
+	public static void main(String[] args){
+        int anoAtual;
+        int idade;
+        int anoNascimento;
+        
+        anoNascimento = 2000;
+        anoAtual = 2020;
+
+        idade = anoAtual - anoNascimento;
+
+        System.out.println("A idade do usuário é: " + idade);
+	}
+}
